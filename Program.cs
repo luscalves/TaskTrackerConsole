573a58686fb29f522a178abd3739c8e52154d6ee
luscalves <lucalves14@hotmail.com>
2026-04-29 17:15:42 -0300

    chore: create initial console project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//JOGAR ESSA PARTE PARA UM SLIDE
+/*
+Console.WriteLine("Meu nome é Lucas ALves");
+Console.WriteLine("Estou estudando C# para me tornar um desenvolvedor backend .NET");
+Console.WriteLine("Bem-vindo ao Task Tracker Console!");
+Console.WriteLine("Hoje estou criando a base do projeto.");
+
+Console.WriteLine("Este projeto será um gerenciador de tarefas \n ele será feito em C# usando console \n" +
+                  "vou evoluir ele durante meus estudos");
+
+Console.WriteLine("O projeto terá:\n-Cadastro de tarefas;\n-Status:pendente, em andamento, concluída;\n" +
+                  "-Filtro por status;-Pesistência em arquivo JSON;\n-README explicando decisões.");
+*/
+//SLIDE
+Console.WriteLine("=======================================");
+Console.WriteLine("        TASK TRACKER CONSOLE           ");
+
+Console.WriteLine("=======================================");
+Console.WriteLine("-----SISTEMA INICIADO COM SUCESSO.-----");
+Console.WriteLine("=======================================");
+
+Console.WriteLine("O objetivo desse projeto console em C# é praticar lógica, Git e Backend\n");
+Console.WriteLine("MENU:");
+Console.WriteLine("01 - Cadastrar tarefa");
+Console.WriteLine("02 - Listar tarefas");
+Console.WriteLine("00 - Sair");
+
+
+                  
